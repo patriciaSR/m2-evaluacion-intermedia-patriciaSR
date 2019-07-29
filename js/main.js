c0eb834c16e61b593d57c19b4dc0f797467c66d3
patriiSR <patricia.suarez.psr@gmail.com>
2019-07-29 09:20:37 +0200

    add counter attemps

--- js/main.js
+++ js/main.js
@@ -2,24 +2,34 @@
 
 const number = document.querySelector('.input__number');
 const btn = document.querySelector('.btn')
-const clue = document.querySelector ('.clue__text')
+const clue = document.querySelector('.clue__text')
+const counter = document.querySelector('.counter');
 
-
+let attemps = 0;
 
 
 function getRandomNumber(max) {
   return Math.ceil(Math.random() * max);
 }
 
-function updateNumber() {
+
+function updateNumber(event) {
   const numberInput = number.value;
+
   if (numberInput > getRandomNumber(100)) {
     clue.innerHTML = 'demasiado alto';
+    attemps++;
+    counter.innerHTML = attemps;
+
   } else if (numberInput < getRandomNumber(100)) {
     clue.innerHTML = 'demasiado bajo';
-  }else {
+    attemps++;
+    counter.innerHTML = attemps;
+  } else {
     clue.innerHTML = '¡HAS GANADO, CAMPEONA!';
   }
+
+
 
 }
 
