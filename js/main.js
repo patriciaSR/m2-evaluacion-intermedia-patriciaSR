945072477e999f573a2c1ff200683042434609f0
patriiSR <patricia.suarez.psr@gmail.com>
2019-07-29 08:40:37 +0200

    initial html structure and js variables

---
+++ js/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const number = document.querySelector('.input__number');
+const btn = document.querySelector('.btn')
+
+
+
+
+function getRandomNumber(max) {
+  return Math.ceil(Math.random() * max);
+}
+
+fuction updateNumber {
+  
+}
+
+btn.addEventListener('click', updateNumber);
