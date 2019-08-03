a653ac715f8816e1bcbc66144322c327bb1ec788
patriiSR <patricia.suarez.psr@gmail.com>
2019-08-03 17:04:13 +0200

    add box alert when number input is negative or bigger than 100

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@ const number = document.querySelector('.input__number');
 const btn = document.querySelector('.btn')
 const clue = document.querySelector('.clue__text')
 const counter = document.querySelector('.counter');
+const alertBox = document.querySelector('.alert-box');
 
 let attemps = 0;
 
@@ -17,6 +18,10 @@ console.log(randomNumber);
 function clueText(text) {
   clue.innerHTML = text;
 }
+function alertText(text) {
+  alertBox.innerHTML = text;
+}
+
 
 function counterResult(num) {
   counter.innerHTML = num;
@@ -25,18 +30,25 @@ function counterResult(num) {
 function updateNumber(event) {
   const numberInput = parseInt(number.value);
 
-  if (numberInput > randomNumber) {
-    clueText('demasiado alto');
-    attemps++;
-    counterResult(attemps);
-  } else if (numberInput < randomNumber) {
-    clueText('demasiado bajo');
-    attemps++;
-    counterResult(attemps);
+  if (numberInput < 0 || numberInput > 100) {
+    alertBox.classList.remove('hidden');
+    alertText('El número introducido tiene que ser entre 0 y 100');
   } else {
-    clueText('¡HAS GANADO, CAMPEONA!');
-    attemps = 0;
-    counterResult(attemps);
+    alertBox.classList.add('hidden');
+
+    if (numberInput > randomNumber) {
+      clueText('demasiado alto');
+      attemps++;
+      counterResult(attemps);
+    } else if (numberInput < randomNumber) {
+      clueText('demasiado bajo');
+      attemps++;
+      counterResult(attemps);
+    } else {
+      clueText('¡HAS GANADO, CAMPEONA!');
+      attemps = 0;
+      counterResult(attemps);
+    }
   }
 }
 
