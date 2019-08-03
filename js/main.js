f14b8b20a3cbe76a36365894300d0b75c5ec633b
patriciaSR <patricia.suarez.psr@gmail.com>
2019-08-03 18:57:15 +0200

    fix number input conditions and add winner cup

--- js/main.js
+++ js/main.js
@@ -1,11 +1,13 @@
 'use strict';
 
+//variables elements
 const number = document.querySelector('.input__number');
 const btn = document.querySelector('.play-btn');
 const resetBtn = document.querySelector('.reset-btn');
 const clue = document.querySelector('.clue__text');
 const counter = document.querySelector('.counter');
 const alertBox = document.querySelector('.alert-box');
+const imgWinner = document.querySelector('.winner__img');
 
 let attemps = 0;
 
@@ -15,6 +17,7 @@ function getRandomNumber(max) {
 
 let randomNumber = getRandomNumber(100);
 console.log(randomNumber);
+
 
 function clueText(text) {
   clue.innerHTML = text;
@@ -30,16 +33,16 @@ function counterResult(num) {
 
 function updateNumber(event) {
   const numberInput = parseInt(number.value);
-
-  if (!numberInput) {
+  
+  if (!numberInput && numberInput !== 0) {
     alertBox.classList.remove('hidden');
-    alertText('Introduce un número para empezar a jugar :)');
+    alertText('Introduce un número para poder jugar :)');
   } else if (numberInput < 0 || numberInput > 100) {
     alertBox.classList.remove('hidden');
     alertText('El número introducido tiene que ser entre 0 y 100');
   } else {
     alertBox.classList.add('hidden');
-
+    
     if (numberInput > randomNumber) {
       clueText('demasiado alto');
       attemps++;
@@ -50,10 +53,10 @@ function updateNumber(event) {
       counterResult(attemps);
     } else {
       clueText('¡HAS GANADO, CAMPEONA!');
-      attemps = 0;
-      counterResult(attemps);
+      clue.classList.add('winner');
+      imgWinner.classList.remove('hidden');
       btn.disabled = true;
-
+      
     }
   }
 }
@@ -70,11 +73,12 @@ function resetGame() {
   counterResult(attemps);
   number.value = "";
   clueText('Escribe un número y dale a Prueba!');
+  imgWinner.classList.add('hidden');
   randomNumber = getRandomNumber(100);
   console.log(randomNumber);
 }
 
-
+//listeners
 btn.addEventListener('click', updateNumber);
 number.addEventListener('keyup', enterKey);
 resetBtn.addEventListener('click', resetGame);
